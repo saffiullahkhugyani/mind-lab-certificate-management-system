2e7dff3ac186f44d0fc7bbc0140d5c279e23e9fe
saffiullahkhugyani <saffiullah.khugyani@gmail.com>
2025-03-02 10:27:53 +0500

    code refactored in action file of login

--- src/lib/actions/action.ts
+++ src/lib/actions/action.ts
@@ -1,7 +1,7 @@
 import { createClientReadOnly } from "../supabase/server";
 
 export async function readUserSession() {
-    const supabase =  createClientReadOnly();
+    const supabase = createClientReadOnly();
 
     return supabase.auth.getSession();
 }
