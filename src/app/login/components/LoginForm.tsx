5c570dcc364f2383d97bb84a77b497f249f238c8
saffiullahkhugyani <saffiullah.khugyani@gmail.com>
2025-04-16 10:43:12 +0500

    changed login text

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -58,7 +58,7 @@ export default function LoginForm() {
     <section className="h-[calc(100vh-57px)] flex justify-center items-center">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-2xl">Admin Sponsor Login</CardTitle>
           <CardDescription>
             Enter your email below to login to your account
           </CardDescription>
