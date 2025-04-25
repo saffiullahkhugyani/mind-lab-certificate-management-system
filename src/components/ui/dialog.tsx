270046fa6219ea63eb6ffeb4c2e51d6cda49bcb4
saffiullahkhugyani <saffiullah.khugyani@gmail.com>
2025-04-25 10:32:02 +0500

    bug fix for production

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -49,8 +49,10 @@ const DialogContent = React.forwardRef<
           asChild
           className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
         >
-          <Button>Close</Button>
-          <span className="sr-only">Close</span>
+          <Button>
+            Close
+            <span className="sr-only">Close</span>
+          </Button>
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogOverlay>
